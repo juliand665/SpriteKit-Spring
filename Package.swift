34c27b5b9d0b0d0cb54dbf00615d137024fc5382
Julian Dunskus <julian.dunskus@gmail.com>
2019-09-25 23:20:10 +0200

    switched to swift package

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+	name: "SpriteKit-Spring",
+	products: [
+		.library(
+			name: "SpriteKit-Spring",
+			targets: ["SpriteKit-Spring"]
+		),
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "SpriteKit-Spring",
+			dependencies: []
+		),
+	]
+)
