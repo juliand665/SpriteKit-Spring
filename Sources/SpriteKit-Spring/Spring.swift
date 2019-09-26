a40d2f670573d1a3d1af6884548c283c123b5230
Julian Dunskus <julian.dunskus@gmail.com>
2019-09-26 23:42:58 +0200

    switched back to less general system
    apparently, SKActionTimingFunction doesn't allow you to overshoot! that was bad news bears for underdamped springs

--- Sources/SpriteKit-Spring/Spring.swift
+++ Sources/SpriteKit-Spring/Spring.swift
@@ -1,16 +1,80 @@
 import SpriteKit
 
 public extension SKAction {
-	func usingSpring(with properties: Spring.AnimationProperties) -> Self {
+	static func animate<Node>(
+		_ keyPath: ReferenceWritableKeyPath<Node, CGFloat>,
+		_ target: ValueTransformation,
+		using settings: SpringAnimationSettings
+	) -> SKAction where Node: SKNode {
+		let function = Spring.timingFunction(with: settings.springProperties)
+		let duration = settings.duration
+		return .customAction(withDuration: duration) { node, elapsedTime in
+			guard let node = node as? Node else {
+				fatalError("node to animate did not conform to specified type")
+			}
+			
+			let value = function(Float(elapsedTime))
+			node[keyPath: keyPath] = CGFloat(value)
+		}
+	}
+	
+	func usingSpring(with properties: Spring.Properties) -> Self {
 		timingFunction = Spring.timingFunction(with: properties)
 		return self
+	}
+	
+	struct SpringAnimationSettings {
+		var duration: TimeInterval
+		var springProperties: Spring.Properties
+		
+		public init(duration: TimeInterval, springProperties: Spring.Properties) {
+			self.duration = duration
+			self.springProperties = springProperties
+		}
+	}
+
+	enum ValueTransformation {
+		case change(to: CGFloat)
+		case add(CGFloat)
+		case multiply(by: CGFloat)
+		
+		func valueApplier<Node>(
+			on keyPath: ReferenceWritableKeyPath<Node, CGFloat>
+		) -> ((Node, CGFloat) -> Void) {
+			switch self {
+			case .change(to: let finalValue): // ends up at finalValue regardless of interference from other sources
+				var _initialValue: CGFloat?
+				return { node, springValue in
+					guard let initialValue = _initialValue else {
+						initialValue = finalValue
+						return
+					}
+					node[keyPath: keyPath] = finalValue + springValue * (finalValue - initialValue)
+				}
+				
+			case .add(let offset): // overall always applies the given offset, also working in parallel with other .add transformations
+				var lastSpringValue: CGFloat = 0.0
+				return { node, springValue in
+					node[keyPath: keyPath] += (springValue - lastSpringValue) * offset
+					lastSpringValue = springValue
+				}
+				
+			case .multiply(by: let factor): // overall always applies the given offset, also working in parallel with other .multiply transformations
+				var lastMultiplier: CGFloat = 1.0
+				return { node, springValue in
+					let multiplier = (factor - 1) * springValue + 1
+					node[keyPath: keyPath] *= multiplier / lastMultiplier
+					lastMultiplier = multiplier
+				}
+			}
+		}
 	}
 }
 
 private typealias FloatLiteralType = Float
 
 public enum Spring {
-	public static func timingFunction(with properties: AnimationProperties) -> SKActionTimingFunction {
+	public static func timingFunction(with properties: Properties) -> (Float) -> Float {
 		// lots of math adapted from http://www.ryanjuckett.com/programming/damped-springs/
 		
 		let damping = Damping(ratio: properties.dampingRatio)
@@ -83,7 +147,7 @@ public enum Spring {
 		}
 	}
 	
-	public struct AnimationProperties {
+	public struct Properties {
 		/**
 		The ratio of the spring's damping coefficient to its critical damping coefficient.
 		
@@ -106,8 +170,8 @@ public enum Spring {
 		Check out the individual properties' documentation for additional information.
 		
 		- Parameters:
-			- dampingRatio: The ratio of the spring's damping coefficient to its critical damping coefficient.
-			- initialVelocity: How fast the spring is initially moving, relative to the total distance to cover and the total duration.
+		- dampingRatio: The ratio of the spring's damping coefficient to its critical damping coefficient.
+		- initialVelocity: How fast the spring is initially moving, relative to the total distance to cover and the total duration.
 		*/
 		public init(dampingRatio: Float, initialVelocity: Float) {
 			self.dampingRatio = dampingRatio
